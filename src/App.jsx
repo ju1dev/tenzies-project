e5e75c42758ebe973cee560070365b7d63f43173
Juan Oliveira <91916239+ju1dev@users.noreply.github.com>
2023-04-17 21:35:16 -0300

    app finalizado
    Adicionado funcionalidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Die from './Die'
 import './App.css'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
+
 function App() {
 
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (allHeld & allSameValue) {
+      setTenzies(true)
+      console.log('You Won !')
+    } else {
+      setTenzies(false)
+    }
+  }, [dice])
 
   function allNewDice() {
     const newDice = []
     for(let i = 0; i < 10; i++) {
-      let randomNumber = Math.floor(Math.random() * 7)
-      newDice.push({value: randomNumber, isHeld: false, id: nanoid()})
+      newDice.push(generateNewDie())
     }
     return newDice
   }
 
-  function holdDice(id) {
-    console.log(id)
+  function generateNewDie() {
+    return ({
+      value: Math.floor(Math.random() * 7), 
+      isHeld: false, 
+      id: nanoid()
+    })
+    }
+
+
+  function rollDice() {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? die : generateNewDie()
+    }))
   }
 
-  console.log(dice)
+  function holdDice(id) {
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
+
+  function winGame() {
+    const winDice = []
+    for(let i = 0; i < 10; i++) {
+      winDice.push({
+      value: 1, 
+      isHeld: true, 
+      id: nanoid()
+      })
+    }
+    return setDice(winDice)
+  }
+  
   const diceElements = dice.map(die => 
   <Die value={die.value}
        isHeld={die.isHeld}
@@ -27,13 +69,23 @@ function App() {
   >
   </Die>
   )
-  console.log(dice)
+  
   return (
     <main>
+      {tenzies == true && <Confetti width={window.innerWidth} height={window.innerHeight}/>}
+    <div className='introduction'>
+      <h1>Tenzies</h1>
+      <p className='description'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+    </div>
+
     <div className="dice-container">
         {diceElements}
     </div>
-    <button className='roll-btn' onClick={()=> setDice(allNewDice())}>Roll</button>
+    {!tenzies ? <button className='roll-btn' onClick={rollDice}>Roll</button> 
+    :
+    <button className='roll-btn' onClick={() => setDice(allNewDice)}>Reset</button>
+    }
+    <button onClick={winGame}></button>
 </main>
   )
 }
